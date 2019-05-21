02adeca39ffc13ee27c36e2509c5219aa6e3dc18
Kanghanna <aa994@naver.com>
2019-05-21 11:20:01 +0900

    spring_lec6

---
+++ Downloads/spring_lec6/src/main/java/com/example/spring_lec6/SpringLec6Application.java
@@ -0,0 +1,13 @@
+package com.example.spring_lec6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLec6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLec6Application.class, args);
+	}
+
+}
